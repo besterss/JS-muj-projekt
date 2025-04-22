b61f20f97a400c07bf73d7c216f29791a7716f86
besters <prochazka@unodor.cz>
2025-04-22 19:31:11 +0200

    nove upravy

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,39 @@
 import { render } from '@czechitas/render';
 import '../global.css';
 import './index.css';
+import './super.css';
+//import { data } from './seznam.json';
+
+const Odkazy = () => {
+  return (
+    <ul>
+      <li><a href="#">Odkaz 1</a></li>
+      <li><a href="#">Odkaz 2</a></li>
+      <li><a href="#">Odkaz 3</a></li>
+      <li><a href="#">Odkaz 4</a></li>
+    </ul>
+  )
+}
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Czechitas, nová patička</p>
+    </footer>
+  );
+}
 
 document.querySelector('#root').innerHTML = render(
   <div className="container">
     <header>
       <div className="logo"></div>
-      <h1>Webová aplikace</h1>
+      <h1>Moje super stránka z druhé lekce</h1>
+      <br /> 
+      <Odkazy />
     </header>
     <main>
-      <p>Startovací šablona pro webovou aplikaci v JavaScriptu s JSX. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-kodim-app">create-kodim-app</a>.</p>
+      <p>Moje naklonovaná stránka.</p>
     </main>
-    <footer>
-      <p>Czechitas, Digitální akademie: Web</p>
-    </footer>
+    <Footer />
   </div>
 );
